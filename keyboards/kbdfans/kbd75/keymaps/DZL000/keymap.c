153031e54f15540fc78edd0143976d7674bdc6a1
DZL000 <davidluo1997@gmail.com>
2022-01-11 06:10:03 +1100

    update keymap and firmware

--- keyboards/kbdfans/kbd75/keymaps/DZL000/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/DZL000/keymap.c
@@ -14,9 +14,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = LAYOUT(
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_NO,  KC_DEL,  _______,  
+    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_NO,  KC_DEL,  _______,
     KC_NO,  KC_ESC ,  C(KC_PGUP),  C(KC_T),  C(KC_PGDN),  LCA(KC_UP),  KC_PGUP,  KC_HOME,  KC_UP  ,  KC_END ,  KC_DEL ,  KC_PSCR,  _______,  _______,            _______,
-    _______,  KC_CAPS,  LCA(KC_LEFT), KC_TAB,  LCA(KC_RGHT),  LCA(KC_DOWN),  KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_INS, _______,                      _______,  _______,
+    G(KC_LSFT),  KC_CAPS,  LCA(KC_LEFT), KC_TAB,  LCA(KC_RGHT),  LCA(KC_DOWN),  KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_INS, _______,                      _______,  _______,
     KC_LSFT,  KC_NO,  C(KC_F8),  C(S(KC_PGUP)),  C(KC_W),  C(S(KC_PGDN)),  KC_RCTL,  KC_APP,  LCA(KC_T),C(KC_A),  _______,  _______,  _______,            _______,  _______,
     _______,  _______,  _______,                      LT(1,KC_SPC),  KC_LCTL,  KC_RALT,                      _______,  _______,  _______,  _______,  _______,  _______
   ),
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RGB_VAI  ,  RGB_VAD  ,  KC_MUTE  ,  KC_VOLU  ,  KC_MPLY  ,  KC_NO  ,  G(KC_PGUP)  ,  G(KC_SCLN)  ,  G(KC_UP)  ,  G(KC_QUOT)  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,            KC_NO  ,
     RGB_M_P  ,  RGB_TOG  ,  KC_MPRV  ,  KC_VOLD  ,  KC_MNXT  ,  KC_NO  ,  G(KC_PGDN)  ,  G(KC_LEFT)  ,  G(KC_DOWN)  ,  G(KC_RGHT)  ,  KC_NO  ,  KC_NO  ,                      KC_NO  ,  KC_NO  ,
     RGB_M_B  ,  KC_NO  ,  RGB_M_R  ,  RGB_M_SW  ,  RGB_M_SN  ,  RGB_M_K  ,  RGB_M_X  ,  RGB_M_G  ,  G(KC_DOT)  ,  KC_NO  ,  G(KC_SLSH)  ,  KC_NO  ,  KC_NO  ,            KC_NO  ,  KC_NO  ,
-    KC_SLEP  ,  KC_NO  ,  KC_NO  ,                      KC_NO  ,  KC_NO  ,  LT(2,KC_SPC)  ,                      KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_WAKE  
+    KC_SLEP  ,  KC_NO  ,  KC_NO  ,                      KC_NO  ,  KC_NO  ,  LT(2,KC_SPC)  ,                      KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_WAKE
   ),
 
   [3] = LAYOUT(
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_WH_U  ,  KC_WH_L  ,  KC_MS_U  ,  KC_WH_R  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,            KC_NO  ,
     LT(3,KC_CAPS)  ,  KC_NO  ,  KC_ACL0  ,  KC_ACL1  ,  KC_ACL2  ,  KC_NO  ,  KC_WH_D  ,  KC_MS_L  ,  KC_MS_D  ,  KC_MS_R  ,  KC_NO  ,  KC_NO  ,                      KC_NO  ,  KC_NO  ,
     KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,            KC_NO  ,  KC_NO  ,
-    KC_NO  ,  KC_NO  ,  KC_NO  ,                      KC_BTN2  ,  KC_BTN1  ,  KC_BTN3  ,                      KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  
+    KC_NO  ,  KC_NO  ,  KC_NO  ,                      KC_BTN2  ,  KC_BTN1  ,  KC_BTN3  ,                      KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO
   ),
 
   [4] = LAYOUT(
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO  ,  KC_AT  ,  KC_PERC  ,  KC_PAST  ,  KC_PSLS  ,  KC_DLR  ,  KC_PAST  ,  KC_P7  ,  KC_P8  ,  KC_P9  ,  KC_PPLS  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,            KC_NO  ,
     KC_UNDS  ,  KC_PIPE  ,  KC_AMPR  ,  KC_EXLM  ,  KC_QUES  ,  KC_CIRC  ,  KC_PSLS  ,  KC_P4  ,  KC_P5  ,  KC_P6  ,  KC_PMNS  ,  KC_NO  ,                      KC_NO  ,  KC_NO  ,
     KC_LCBR  ,  KC_NO  ,  KC_RCBR  ,  KC_PPLS  ,  KC_MINS  , KC_HASH  ,  KC_TILD  ,  KC_NLCK  ,  KC_P1  ,  KC_P2  ,  KC_P3  ,  KC_PENT  ,  KC_NO  ,            KC_NO  ,  KC_NO  ,
-    KC_LBRC  ,  KC_RBRC  ,   KC_GRV ,                      KC_PEQL  ,  KC_P0  ,  KC_PDOT  ,                      KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  
+    KC_LBRC  ,  KC_RBRC  ,   KC_GRV ,                      KC_PEQL  ,  KC_P0  ,  KC_PDOT  ,                      KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO
   ),
 };
 
